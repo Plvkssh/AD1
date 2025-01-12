c3d48cd66fc056a11197ac50cbb790b44cf0b58b
Plvkssh <kksmartregion@gmail.com>
2025-01-12 17:47:15 +0700

    Update ConfigLoader.java

--- src/main/java/util/ConfigLoader.java
+++ src/main/java/util/ConfigLoader.java
@@ -8,16 +8,20 @@ public class ConfigLoader {
 
     static {
         try {
+            // Загружаем конфигурацию из файла
             properties.load(ConfigLoader.class.getClassLoader().getResourceAsStream("config.properties"));
         } catch (IOException e) {
+            // Бросаем RuntimeException с понятным сообщением
             throw new RuntimeException("Не удалось загрузить конфигурационный файл!", e);
         }
     }
 
+    // Получаем значение срока действия
     public static int getDefaultExpiryDays() {
         return Integer.parseInt(properties.getProperty("defaultExpiryDays", "1"));
     }
 
+    // Получаем значение лимита кликов
     public static int getDefaultClickLimit() {
         return Integer.parseInt(properties.getProperty("defaultClickLimit", "10"));
     }
