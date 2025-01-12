8af72e9a8c8bef7ccc962adad9bf4cb88af5b621
Plvkssh <kksmartregion@gmail.com>
2025-01-12 18:05:38 +0700

    Update User.java

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,13 +1,29 @@
-package model;
+package models;
+
+import java.util.UUID;
 
 public class User {
-    private final String username;
+    private String username;
+    private String uuid;
 
     public User(String username) {
         this.username = username;
+        this.uuid = UUID.randomUUID().toString();
     }
 
     public String getUsername() {
         return username;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", uuid='" + uuid + '\'' +
+                '}';
     }
 }
